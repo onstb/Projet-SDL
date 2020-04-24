00e764a71aea4ef05d40757624d7160479a6af09
onstb <47973578+onstb@users.noreply.github.com>
2020-04-24 20:20:33 +0100

    Add files via upload

---
+++ def.h
@@ -0,0 +1,20 @@
+#ifndef DS_H_INCLUDED
+#define DS_H_INCLUDED
+
+
+   typedef struct Input
+  {
+    int left,right,jump,acction,down;
+    
+  } Input;
+
+void initialiser_input (Input *I);
+
+
+
+
+
+
+  #endif // DS_H_INCLUDED
+
+
